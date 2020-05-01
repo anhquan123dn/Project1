9564774c3d9103f21bdf5c55cc5e5e6586594f2f
anhquan123dn <taquansict@gmail.com>
2020-05-01 23:18:02 +0700

    1/5

---
+++ public/js/main.js
@@ -0,0 +1,12 @@
+var mySwiper = new Swiper(".swiper-container", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+  },
+  loop: true,
+  autoplay: true,
+});
