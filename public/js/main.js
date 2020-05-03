44b783008d15db194a0c117c8b5fa9b6a2cd4104
anhquan123dn <taquansict@gmail.com>
2020-05-03 14:31:55 +0700

    3/5

--- public/js/main.js
+++ public/js/main.js
@@ -10,3 +10,11 @@ var mySwiper = new Swiper(".swiper-container", {
   loop: true,
   autoplay: true,
 });
+function flip_vertical_right(x) {
+  x.style.animation =
+    "flip-vertical-right 1.5s cubic-bezier(0.455, 0.030, 0.515, 0.955) both";
+}
+function normal(x) {
+  x.style.animation =
+    "flip-vertical-back 1.5s cubic-bezier(0.455, 0.030, 0.515, 0.955) both";
+}
